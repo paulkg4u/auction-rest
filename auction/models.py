7f4ea5a41968092b40fbdf69793be22cc32d03ab
Paul K. George <paul.george@kingslearning.in>
2018-03-13 13:48:57 +0530

    added models

--- auction/models.py
+++ auction/models.py
@@ -4,3 +4,23 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+class Bidder(models.Model):
+	name = models.CharField(max_length=250)
+	email = models.CharField(max_length=250)
+
+	def __unicode__(self):
+		return self.name
+
+class AuctionItem(models.Model):
+	itemName = models.CharField(max_length=200)
+	itemDescription = models.TextField(default="")
+	startTime = models.DateTimeField()
+	endTime = models.DateTimeField()
+	winner = models.ForeignKey(Bidder, blank=True, null=True)
+	bidAmount = models.IntegerField(default=0)
+	currentBid = models.IntegerField(default=0)
+	itemImage = models.ImageField()
+
+	def __unicode__(self):
+		return  self.itemName
--- auctionRest/settings.py
+++ auctionRest/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'auction.apps.AuctionConfig',
+	'rest_framework',
+	'auction'
 ]
 
 MIDDLEWARE = [
