d0569f4c3ba3e08e1531a6bea41f85b5a635e8e2
Paul K. George <paul.george@kingslearning.in>
2018-03-19 14:52:36 +0530

    changed id to pk

--- auction/urls.py
+++ auction/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
 	# url('^item/(?P<id>[0-9-])$', auction_details, name = "auction_details"),
 	url('^$', AuctionList.as_view(), name = 'auction_list'),
 	url('^item$', AuctionList.as_view(), name = 'auction_list'),
-	url('^item/(?P<id>[0-9-])$', AuctionDetail.as_view(), name = 'auction_detail'),
+	url('^item/(?P<pk>[0-9-])$', AuctionDetail.as_view(), name = 'auction_detail'),
 	url('^item/(?P<id>[0-9-])/bid$', BidView.as_view(), name = 'bid_view')
 ]
